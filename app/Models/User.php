faa1cd551b3b94e90f13d6920be55ec1c2439a5e
NIkhil Patil <85491946+Nikks2908@users.noreply.github.com>
2025-08-25 14:19:54 +0530

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable {
-    protected $table = 'user'; // singular per assignment
+    protected $table = 'user';
     protected $fillable = ['username','password'];
     protected $hidden   = ['password','remember_token'];
     public function getAuthIdentifierName() { return 'id'; }
-    // Username-based auth:
+
     public function getAuthIdentifier() { return $this->getKey(); }
 }
 
